7e00de3859d2692397084d26aeceace02f140457
christopher mikkelsen <christophermikkelsen@live.dk>
2025-01-17 22:16:02 +0100

    Added Report and TafReport Created createReportTime()

---
+++ MetarTaf_Backend/Models/TafReport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Taf.Decoder.entity;
+
+namespace MetarTaf_Backend.Models
+{
+    internal class TafReport : Report
+    {
+        DecodedTaf decodedTaf { get; set; }
+        public TafReport(DecodedTaf decodedTaf)
+        {
+            this.decodedTaf = decodedTaf;
+            base.reportTime = base.createReportTime(decodedTaf.Day.Value, decodedTaf.Time);
+        }
+
+    }
+}
--- MetarTaf_Backend/Program.cs
+++ MetarTaf_Backend/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using MetarTaf_Backend.Models;
+using System.Text;
+using Taf.Decoder;
+using Taf.Decoder.entity;
+
+//TAF skal tilføjes foran den rå streng hvis det ikke står der i forvejen.
+DecodedTaf decodedTaf = TafDecoder.ParseWithMode("TAF AMD EKCH 170327Z 1703/1803 26010KT 9999 BKN008 TEMPO 1706/1708 2000 BR BKN002 TEMPO 1708/1715 4000 BR BKN012 TEMPO 1715/1803 4000 BR BKN004=", true);
+
+TafReport tafReport = new TafReport(decodedTaf);
+
+Console.WriteLine(tafReport.reportTime);
+
+Console.ReadKey();
