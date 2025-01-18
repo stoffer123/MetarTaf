bf26e03ecc69ebd871c659c44195db54ef1882a8
christopher mikkelsen <christophermikkelsen@live.dk>
2025-01-18 21:57:45 +0100

    AirportInfoStation WIP

---
+++ MetarTaf_Backend/Models/AirportInfoStation.cs
@@ -0,0 +1,43 @@
+﻿using MetarTaf_Backend.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetarTaf_Backend.Models
+{
+    internal class AirportInfoStation : IInfoStation
+    {
+        List<IAirport> observers;
+        MetarService metarService = new();
+        
+        public void addObserver(IAirport observer)
+        {
+            observers.Add(observer);
+        }
+        public void removeObserver(IAirport observer)
+        {
+            observers.RemoveAll(observer);
+        }
+
+        public void notifyAirportInfoChange()
+        {
+            foreach (IAirport observer in observers)
+            {
+                observer.updateAirportInfo();
+            }
+        }
+
+        public void notifyMetarChange()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void notifyTafChange()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ MetarTaf_Backend/Models/IInfoStation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetarTaf_Backend.Models
+{
+    internal interface IInfoStation
+    {
+        void removeObserver(IAirport observer);
+        void addObserver(IAirport observer);
+        void notifyTafChange();
+        void notifyMetarChange();
+        void notifyAirportInfoChange();
+    }
+}
