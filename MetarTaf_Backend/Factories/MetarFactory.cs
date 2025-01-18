6c1cdb61f48a668c081bcbf2d10f4fc356cfde55
christopher mikkelsen <christophermikkelsen@live.dk>
2025-01-18 08:21:03 +0100

    Added metarFactory Added TafFactory

---
+++ MetarTaf_Backend/Factories/MetarFactory.cs
@@ -0,0 +1,26 @@
+﻿using Metar.Decoder;
+using Metar.Decoder.Entity;
+using MetarTaf_Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetarTaf_Backend.Factories
+{
+    internal class MetarFactory
+    {
+        public MetarFactory() { }
+
+        public MetarReport createMetar(string metarString)
+        {
+            DecodedMetar decodedMetar = MetarDecoder.ParseWithMode(metarString);
+
+            MetarReport metarReport = new(decodedMetar);
+
+            return metarReport;
+        }
+    }
+}
