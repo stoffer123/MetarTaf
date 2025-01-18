d148f5462babd291f9f32ba82c9ac5ce0e87efae
christopher mikkelsen <christophermikkelsen@live.dk>
2025-01-18 09:24:50 +0100

    WIP AirportController

---
+++ MetarTaf_Backend/AirportController.cs
@@ -0,0 +1,23 @@
+﻿using MetarTaf_Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetarTaf_Backend
+{
+    public static class AirportController
+    {
+        private static Dictionary<string, IAirport> airports = new();
+
+
+
+        public static List<string> getAirportIcaoList()
+        {
+            List<string> strings = airports.Keys.ToList();
+
+            return strings;
+        }
+    }
+}
---
+++ MetarTaf_Backend/Models/IAirport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetarTaf_Backend.Models
+{
+    internal interface IAirport
+    {
+        void updateMetars();
+        void updateTafs();
+        void updateAirportInfo();
+    }
+}
