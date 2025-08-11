ad42a9d706d079fb923c841b15f87d3db3502c34
Christopher <christophermikkelsen@live.dk>
2025-08-11 16:56:42 +0200

    79 sidst indtastede lufthavnen forsvinder ved refresh (#83)
    * Fixed hvornår airports gemmes
    
    * Quickfix til at release airports

--- MetarTaf/Components/Pages/OverviewPage.razor.cs
+++ MetarTaf/Components/Pages/OverviewPage.razor.cs
@@ -76,8 +76,8 @@ namespace MetarTaf.Components.Pages
                     // Check if the airport has valid data
                     if (airport != null)
                     {
-                        await SaveAirportsToLocalStorage();
                         airports.Add(airport);
+                        await SaveAirportsToLocalStorage();
                         StateHasChanged();
 
                     }
@@ -179,6 +179,11 @@ namespace MetarTaf.Components.Pages
 
         public void Dispose()
         {
+            // slip alle subscriptions for denne side
+            foreach (var a in airports.ToArray())
+                airportController.releaseAirport(a.getAirportInfo().icaoId);
+
+            airports.Clear();
             timer?.Dispose();
         }
     }
