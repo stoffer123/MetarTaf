48cf89f0e791188b63e0e268b8c3ae385e130b97
stoffer123 <christophermikkelsen@live.dk>
2025-08-25 11:05:27 +0200

    Lavet domæne lag

---
+++ Domain/Ports/IOpmetFetcher.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Ports
+{
+    /// <summary>
+    /// Port for at hente seneste OPMET pr. ICAO. Adapter i Infrastructure implementerer denne.
+    /// </summary>
+    public interface IOpmetFetcher
+    {
+        /// <returns>
+        /// Tuple af (metarMap, tafMap) hvor værdi er komplette rå linjer,
+        /// eller – alternativt – returnér allerede mappet til OpmetReport (se nedenfor).
+        /// </returns>
+        Task<(Dictionary<string, string> Metar, Dictionary<string, string> Taf)>
+            GetLatestPerIcaoRawAsync(IEnumerable<string> icaos, int windValidTime = 0, CancellationToken ct = default);
+
+        /// <summary>Valgfrit: en port der returnerer domæneobjekter direkte.</summary>
+        Task<IReadOnlyList<OpmetReport>> GetLatestPerIcaoAsync(IEnumerable<string> icaos, int windValidTime = 0, CancellationToken ct = default);
+    }
+}
