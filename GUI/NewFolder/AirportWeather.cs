5906ab76a5194442cf4785a70bc0992e3b1461d4
stoffer123 <christophermikkelsen@live.dk>
2025-12-11 21:23:20 +0100

    MudStuff

---
+++ GUI/NewFolder/AirportWeather.cs
@@ -0,0 +1,12 @@
+﻿namespace GUI.NewFolder
+{
+    public class AirportReport
+    {
+        public string AirportName { get; set; } = string.Empty;
+        public string ReportType { get; set; } = string.Empty; // "METAR" eller "TAF"
+        public DateTime ReportTime { get; set; } //When the report was issued
+        public DateTime FetchTime { get; set; } //When the report was fetched
+        public string RawReport { get; set; } = string.Empty;
+
+    }
+}
---
+++ GUI/NewFolder/WeatherReport.cs
@@ -0,0 +1,9 @@
+﻿namespace GUI.NewFolder
+{
+    public class WeatherReport
+    {
+        public DateTime TimeStamp { get; set; }
+        public string RawReport { get; set; } = string.Empty;
+        public string ReportType { get; set; } = string.Empty; //METAR, TAF, TAF AMD, SPECI osv
+    }
+}
